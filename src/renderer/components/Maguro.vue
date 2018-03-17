0682396a25653c94d3dc649a5cfa803470d4e334
ktr0328 <zf7heatpa@gmail.com>
2018-03-17 10:57:51 +0900

    First Commit

---
+++ src/renderer/components/Maguro.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="maguro"><div>
+</template>
+
+<script>
+import P5 from 'p5'
+
+export default {
+  name: 'maguro',
+  data () {
+
+  },
+  methods: {
+    sketch: function (p) {
+      p.setup = () => p.createCanvas(document.body.clientWidth, document.body.clientHeight)
+
+      p.draw = () => {
+      }
+    }
+  },
+  mounted: function () {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
